345a4c381f43f99e83761de9da3743fefbb18cba
donghyeon <gmj49@naver.com>
2025-07-29 17:35:30 +0900

    feat: 기능 5개 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,26 @@
 package lotto.service;
 
+import java.util.List;
+
 public class LottoService {
+
+    public int buyLotto(int pay) {
+    
+    }
+    
+    public void issueLotto(int lottiesCount) {
+    
+    }
+    
+    public void confirmWinning(List<Integer> winningLotto) {
+    
+    }
+    
+    public void compileStatistics(int[] rank) {
+    
+    }
+    
+    public float findTheRateOfReturn(long toalWinnigs) {
+    
+    }
 }
