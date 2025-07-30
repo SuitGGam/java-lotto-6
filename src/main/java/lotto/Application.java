ff6d111fa5d9d904330fc76996c6c71abeb6e6f1
donghyeon <gmj49@naver.com>
2025-07-30 17:52:50 +0900

    feat: main 작동 코드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,38 @@
 package lotto;
 
+import lotto.domain.LottoConstants;
+import lotto.service.LottoService;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        LottoService lottoService = new LottoService();
+        LottoConstants  lottoConstants  = new LottoConstants();
+        LottoInputView  lottoInputView  = new LottoInputView();
+        LottoOutputView lottoOutputView = new LottoOutputView();
+        
+        lottoInputView.inputProcessPay();
+        String pay = br.readLine();
+        System.out.println();
+        List<Integer>[] issuedLotties = lottoService.runBuy(pay);
+        lottoOutputView.outputProcessIssuedLotties(issuedLotties);
+        lottoInputView.inputProcessWinningLotto();
+        String winningLotto = br.readLine();
+        System.out.println();
+        lottoInputView.inputProcessBonusNumber();
+        String bonusNumber = br.readLine();
+        System.out.println();
+        int[] rank = lottoService.runConfirm(winningLotto, bonusNumber, issuedLotties);
+        lottoOutputView.outputProcessStatistics(issuedLotties.length * lottoConstants.LOTTO_PRICE, rank);
+        
+        br.close();
     }
 }
