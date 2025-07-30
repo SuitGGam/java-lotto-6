42b7274633e904a44803d601aded06f76f76fe5f
donghyeon <gmj49@naver.com>
2025-07-30 17:52:32 +0900

    feat: 입출력, 전처리 기능 추가

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -6,7 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoInputView {
-
+    
+    public void inputProcessPay() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    
+    public void inputProcessWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    
+    public void inputProcessBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+    
     public int preProcessPay(String pay) {
         pay = pay.replaceAll(",", "");
         pay = pay.replaceAll("원", "");
--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -1,4 +1,39 @@
 package lotto.view;
 
+import lotto.service.LottoService;
+
+import java.util.List;
+
 public class LottoOutputView {
+
+    public void outputProcessIssuedLotties(List<Integer>[] issuedLotties) {
+        StringBuilder sb = new StringBuilder();
+        int len = issuedLotties.length;
+        
+        sb.append(len).append("개를 구매했습니다.").append("\n");
+        for (int i = 0; i < len; i++) {
+            sb.append(issuedLotties[i].toString()).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+    
+    public void outputProcessStatistics(int pay, int[] rank) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("당첨 통계\n").append("---\n");
+        sb.append("3개 일치 (5,000원) - ").append(rank[0]).append("개\n");
+        sb.append("4개 일치 (50,000원) - ").append(rank[1]).append("개\n");
+        sb.append("5개 일치 (1,500,000원) - ").append(rank[2]).append("개\n");
+        sb.append("5개 일치, 보너스 볼 일치 (30,000,000원) - ").append(rank[3]).append("개\n");
+        sb.append("6개 일치 (2,000,000,000원) - ").append(rank[4]).append("개\n");
+        
+        LottoService lottoService = new LottoService();
+        long totalWinning = lottoService.compileStatistics(rank);
+        
+        double theRateOfReturn = lottoService.findTheRateOfReturn(pay, totalWinning);
+        sb.append("총 수익률은 ").append(theRateOfReturn).append("%입니다.");
+        
+        System.out.print(sb);
+    }
 }
