aeb6fe25c6b04fc2fedbca351791509b4ab36af4
donghyeon <gmj49@naver.com>
2025-08-01 16:46:19 +0900

    feat: 입력 전처리 기능 분리

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -18,31 +18,4 @@ public class LottoInputView {
     public void inputProcessBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
     }
-    
-    public int preProcessPay(String pay) {
-        pay = pay.replaceAll(",", "");
-        pay = pay.replaceAll("원", "");
-        
-        int value = Integer.parseInt(pay);
-        return value;
-    }
-    
-    public List<Integer> preProcessWinningLotto(String numbers) {
-        numbers = numbers.replaceAll(" ", "");
-        String[] winningNumbers = numbers.split(",");
-        
-        List<Integer> winningLotto = new ArrayList<>();
-        for (int i = 0; i < LottoConstants.LOTTO_NUMBER_COUNT; i++) {
-            winningLotto.add(Integer.parseInt(winningNumbers[i]));
-        }
-        
-        return winningLotto;
-    }
-    
-    public List<Integer> preProcessBonusNumber(String number) {
-        List<Integer> bonusNumber = new ArrayList<>();
-        bonusNumber.add(Integer.parseInt(number));
-        
-        return bonusNumber;
-    }
 }
