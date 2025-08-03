cecd07749ff365315ca5494b24be8c22a229fd19
donghyeon <gmj49@naver.com>
2025-08-04 03:42:49 +0900

    refactor: 중복 검사 최적화

--- src/main/java/lotto/validation/LottoNumbersValidation.java
+++ src/main/java/lotto/validation/LottoNumbersValidation.java
@@ -5,7 +5,9 @@ import lotto.exception.LottoNumberCountException;
 import lotto.exception.LottoNumberDuplicatedException;
 import lotto.exception.LottoNumberRangeException;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoNumbersValidation {
 
@@ -28,12 +30,9 @@ public class LottoNumbersValidation {
     }
 
     public boolean areDuplicated(List<Integer> lottoNumbers) {
-        for (int i = 0; i < LottoConstants.LOTTO_NUMBER_COUNT; i++) {
-            for (int j = 0; j < LottoConstants.LOTTO_NUMBER_COUNT; j++) {
-                if (i != j && lottoNumbers.get(i) == lottoNumbers.get(j)) {
-                    throw new LottoNumberDuplicatedException("[ERROR] 로또 번호에 중복이 있으면 안 됩니다.");
-                }
-            }
+        Set<Integer> uniqueNumbers = new HashSet<>(lottoNumbers);
+        if (uniqueNumbers.size() != lottoNumbers.size()) {
+            throw new LottoNumberDuplicatedException("[ERROR] 로또 번호에 중복이 있으면 안 됩니다.");
         }
 
         return true;
