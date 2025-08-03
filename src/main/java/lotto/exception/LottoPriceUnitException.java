86c20c88592ad6c89b537cdbc230d61ae0de0d7e
donghyeon <gmj49@naver.com>
2025-08-03 22:03:46 +0900

    refactor: Unit 클래스 이름 수정

---
+++ src/main/java/lotto/exception/LottoPriceUnitException.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public class LottoPriceUnitException extends RuntimeException {
+    public LottoPriceUnitException(String message) {
+        super(message);
+    }
+}
--- src/main/java/lotto/exception/LottoUnitException.java
+++
@@ -1,7 +0,0 @@
-package lotto.exception;
-
-public class LottoUnitException extends RuntimeException {
-    public LottoUnitException(String message) {
-        super(message);
-    }
-}
